0924749007b64b1db7b8b2916b7e18f35ab76428
Stanislav Petr <petr@digitaldata.cz>
2021-09-04 22:52:07 +0200

    Service

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/spetr/zabrbl
 
 go 1.12
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/kardianos/service v1.2.0
+	gopkg.in/yaml.v2 v2.4.0
+)
--- main.go
+++ main.go
@@ -2,37 +2,112 @@ package main
 
 import (
 	"encoding/binary"
+	"flag"
 	"fmt"
+	"log"
 	"net"
 
+	"github.com/kardianos/service"
 	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	ConfLoad()
+type program struct {
+	exit chan struct{}
+}
 
-	for i := range conf.IP {
-		// Single IP
-		if ip := net.ParseIP(conf.IP[i]); ip != nil {
-			r := rblLookup(conf.RBL.IPv4, ip)
-			x, _ := yaml.Marshal(r)
-			fmt.Println(string(x))
-			continue
-		}
-		// CIDR
-		if _, cidr, err := net.ParseCIDR(conf.IP[i]); err == nil {
-			mask := binary.BigEndian.Uint32(cidr.Mask)
-			start := binary.BigEndian.Uint32(cidr.IP)
-			finish := (start & mask) | (mask ^ 0xffffffff)
-			for i := start; i <= finish; i++ {
-				ip := make(net.IP, 4)
-				binary.BigEndian.PutUint32(ip, i)
+var (
+	logger service.Logger
+)
+
+func (p *program) Start(s service.Service) error {
+	if service.Interactive() {
+		logger.Info("Running in terminal.")
+	} else {
+		logger.Info("Running under service manager.")
+	}
+	p.exit = make(chan struct{})
+	go func() {
+		ConfLoad()
+		for i := range conf.IP {
+			// Single IP
+			if ip := net.ParseIP(conf.IP[i]); ip != nil {
 				r := rblLookup(conf.RBL.IPv4, ip)
 				x, _ := yaml.Marshal(r)
 				fmt.Println(string(x))
+				continue
 			}
-			continue
+			// CIDR
+			if _, cidr, err := net.ParseCIDR(conf.IP[i]); err == nil {
+				mask := binary.BigEndian.Uint32(cidr.Mask)
+				start := binary.BigEndian.Uint32(cidr.IP)
+				finish := (start & mask) | (mask ^ 0xffffffff)
+				for i := start; i <= finish; i++ {
+					ip := make(net.IP, 4)
+					binary.BigEndian.PutUint32(ip, i)
+					r := rblLookup(conf.RBL.IPv4, ip)
+					x, _ := yaml.Marshal(r)
+					fmt.Println(string(x))
+				}
+				continue
+			}
 		}
+	}()
+	return nil
+}
+
+func (p *program) Stop(s service.Service) error {
+	logger.Info("Stopping")
+	close(p.exit)
+	return nil
+}
+
+func main() {
+	svcFlag := flag.String("service", "", "Control the system service.")
+	flag.Parse()
+
+	options := make(service.KeyValue)
+	options["Restart"] = "on-success"
+	options["SuccessExitStatus"] = "1 2 8 SIGKILL"
+	svcConfig := &service.Config{
+		Name:        "ZabRBL",
+		DisplayName: "ZabRBL monitoring service",
+		Description: "RBL monitoring service for Zabbix.",
+		Dependencies: []string{
+			"Requires=network.target",
+			"After=network-online.target syslog.target"},
+		Option: options,
 	}
 
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errs := make(chan error, 5)
+	logger, err = s.Logger(errs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			err := <-errs
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	}()
+
+	if len(*svcFlag) != 0 {
+		err := service.Control(s, *svcFlag)
+		if err != nil {
+			log.Printf("Valid actions: %q\n", service.ControlAction)
+			log.Fatal(err)
+		}
+		return
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
 }
