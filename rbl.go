d028cf363ad81f15b2d0abf0e661c30b55beb385
Stanislav Petr <petr@digitaldata.cz>
2021-09-06 15:51:49 +0200

    code cleanup

--- rbl.go
+++ rbl.go
@@ -24,70 +24,7 @@ type Result struct {
 	Text   string `json:"text,omitempty" yaml:"text,omitempty"`
 }
 
-func rblQuery(ip net.IP, rbl string) (r *Result) {
-
-	// Reverse IPv4 address
-	splitAddress := strings.Split(ip.String(), ".")
-	for i, j := 0, len(splitAddress)-1; i < len(splitAddress)/2; i, j = i+1, j-1 {
-		splitAddress[i], splitAddress[j] = splitAddress[j], splitAddress[i]
-	}
-
-	lookup := fmt.Sprintf("%s.%s", strings.Join(splitAddress, "."), rbl)
-	r = &Result{
-		Listed: false,
-		Rbl:    rbl,
-	}
-	regexpResponse, _ := regexp.Compile(`^127\.0\.0\.*`)
-
-	c := &dns.Client{
-		DialTimeout: time.Millisecond * 1500,
-	}
-
-	m := &dns.Msg{
-		MsgHdr: dns.MsgHdr{
-			RecursionDesired: true,
-		},
-		Compress: true,
-	}
-
-	m.SetQuestion(dns.Fqdn(lookup), dns.TypeA)
-	dnsResponse, _, err := c.Exchange(m, "1.1.1.1:53")
-	if err != nil {
-		logger.Error(err.Error())
-	}
-
-	if dnsResponse != nil && dnsResponse.Rcode == dns.RcodeSuccess {
-		for i := range dnsResponse.Answer {
-			if a, ok := dnsResponse.Answer[i].(*dns.A); ok {
-				if regexpResponse.MatchString(a.A.String()) {
-					r.Listed = true
-					break
-				}
-			}
-		}
-	}
-
-	if r.Listed {
-		m.SetQuestion(dns.Fqdn(lookup), dns.TypeTXT)
-		dnsResponse, _, err := c.Exchange(m, "8.8.8.8:53")
-		if err != nil {
-			logger.Error(err.Error())
-		}
-		if dnsResponse != nil && dnsResponse.Rcode == dns.RcodeSuccess {
-			for i := range dnsResponse.Answer {
-				if a, ok := dnsResponse.Answer[i].(*dns.TXT); ok {
-					r.Text = strings.Join(a.Txt, " ")
-					break
-				}
-			}
-		}
-		reportWriteLine(ip, r.Rbl, r.Text)
-	}
-
-	return
-}
-
-func rblLookup(rblList []string, ip net.IP) (res *RBLResults) {
+func rblLookup(ip net.IP, rblList []string) (res *RBLResults) {
 	var wg sync.WaitGroup
 	res = &RBLResults{
 		IP: ip,
@@ -98,7 +35,65 @@ func rblLookup(rblList []string, ip net.IP) (res *RBLResults) {
 			defer func() {
 				wg.Done()
 			}()
-			r := rblQuery(ip, rblList[i])
+
+			// Reverse IPv4 address
+			splitAddress := strings.Split(ip.String(), ".")
+			for i, j := 0, len(splitAddress)-1; i < len(splitAddress)/2; i, j = i+1, j-1 {
+				splitAddress[i], splitAddress[j] = splitAddress[j], splitAddress[i]
+			}
+
+			lookup := fmt.Sprintf("%s.%s", strings.Join(splitAddress, "."), rblList[i])
+			r := &Result{
+				Listed: false,
+				Rbl:    rblList[i],
+			}
+			regexpResponse, _ := regexp.Compile(`^127\.0\.0\.*`)
+
+			c := &dns.Client{
+				DialTimeout: time.Millisecond * 1500,
+			}
+
+			m := &dns.Msg{
+				MsgHdr: dns.MsgHdr{
+					RecursionDesired: true,
+				},
+				Compress: true,
+			}
+
+			m.SetQuestion(dns.Fqdn(lookup), dns.TypeA)
+			dnsResponse, _, err := c.Exchange(m, "1.1.1.1:53")
+			if err != nil {
+				logger.Error(err.Error())
+			}
+
+			if dnsResponse != nil && dnsResponse.Rcode == dns.RcodeSuccess {
+				for i := range dnsResponse.Answer {
+					if a, ok := dnsResponse.Answer[i].(*dns.A); ok {
+						if regexpResponse.MatchString(a.A.String()) {
+							r.Listed = true
+							break
+						}
+					}
+				}
+			}
+
+			if r.Listed {
+				m.SetQuestion(dns.Fqdn(lookup), dns.TypeTXT)
+				dnsResponse, _, err := c.Exchange(m, "8.8.8.8:53")
+				if err != nil {
+					logger.Error(err.Error())
+				}
+				if dnsResponse != nil && dnsResponse.Rcode == dns.RcodeSuccess {
+					for i := range dnsResponse.Answer {
+						if a, ok := dnsResponse.Answer[i].(*dns.TXT); ok {
+							r.Text = strings.Join(a.Txt, " ")
+							break
+						}
+					}
+				}
+				reportWriteLine(ip, r.Rbl, r.Text)
+			}
+
 			res.Results = append(res.Results, r)
 		}(&wg, i)
 	}
@@ -110,7 +105,7 @@ func rblLookupList(rblList []string, ip []string) {
 	for i := range ip {
 		// Single IP
 		if ip := net.ParseIP(ip[i]); ip != nil {
-			r := rblLookup(rblList, ip)
+			r := rblLookup(ip, rblList)
 			x, _ := yaml.Marshal(r)
 			fmt.Println(string(x))
 			continue
@@ -123,7 +118,7 @@ func rblLookupList(rblList []string, ip []string) {
 			for i := start; i <= finish; i++ {
 				ip := make(net.IP, 4)
 				binary.BigEndian.PutUint32(ip, i)
-				r := rblLookup(rblList, ip)
+				r := rblLookup(ip, rblList)
 				x, _ := yaml.Marshal(r)
 				fmt.Println(string(x))
 			}
